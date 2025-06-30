13fdb20ff4a7c8acccd541f115ef3db49dd7e03a
PiotrBzdrega <piotrbzdrega@yandex.com>
2025-06-30 17:56:15 +0200

    string_view

--- string_view.cpp
+++ string_view.cpp
@@ -1,12 +1,25 @@
 #include <cstdio>
 #include <string>
-
+#include <print>
 
 void string_view_value(std::string_view str)
 {
     std::printf("%s\n",str.data());
 }
 
+struct string_view_in_cstr
+{
+    std::string_view _view;
+    string_view_in_cstr(std::string_view view) : _view(view)
+    {
+
+    }
+
+    void print_view()
+    {
+        std::println("{}",_view);
+    }
+};
 
 int main()
 {
@@ -14,5 +27,8 @@ int main()
     std::printf("####################\n\n");
     std::string string{"adamek"};
     string_view_value(string);
+    auto instance = string_view_in_cstr("abc");
+    instance.print_view();
+
     return 0;
 }
